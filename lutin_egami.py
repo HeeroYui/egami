f88b8e8e92abbb7188e267fb90cf304eaa7c846e
Edouard DUPIN <yui.heero@gmail.com>
2015-05-08 22:36:40 +0200

    [BUILD] update new lutin 0.5.3

--- lutin_egami.py
+++ lutin_egami.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
-import lutinModule as module
-import lutinTools as tools
-import lutinTools
+import lutin.module as module
+import lutin.tools as tools
 
 def get_desc():
 	return "e-gami library (image generator from multiple image type)"
@@ -26,12 +25,11 @@ def create(target):
 	# name of the dependency
 	myModule.add_module_depend(['etk', 'png', 'esvg', 'edtaa3'])
 	
-	myModule.compile_flags_CC([
+	myModule.compile_flags('c++', [
 		'-Wno-write-strings',
 		'-Wall'])
 	
 	myModule.add_export_path(tools.get_current_path(__file__))
-	
 	
 	# add the currrent module at the 
 	return myModule
